2a1ee81105a8789be5b828e908e708c0f8ce1e0b
SHJewell <scott.hjewell@gmail.com>
2024-02-08 17:55:39 -0800

    Added spherical cosines function.
    Can now calculate surface lat-lon of groundtracks.

--- main.py
+++ main.py
@@ -7,19 +7,25 @@ from matplotlib import pyplot as plt
 # Useful for defining quantities
 from astropy import units as u
 from astropy.time import Time
+from astropy.coordinates import (
+    GCRS,
+    ITRS,
+    SphericalRepresentation,
+    CartesianRepresentation,
+    CartesianDifferential
+)
 
-# Earth focused modules, ISS example orbit and time span generator
 from poliastro.earth import EarthSatellite
-from poliastro.earth.plotting import GroundtrackPlotter
 from poliastro.twobody import Orbit
-from poliastro.twobody.propagation import cowell
-from poliastro.examples import iss
 from poliastro.util import time_range
 from poliastro.core.spheroid_location import cartesian_to_ellipsoidal
 from poliastro.bodies import Earth
-from poliastro.twobody.propagation import propagate
+from poliastro.twobody.sampling import EpochsArray
 
-def def_orb():
+
+
+def def_iss_orb():
+    from poliastro.examples import iss
     # Build spacecraft instance
     iss_spacecraft = EarthSatellite(iss, None)
     # t_span = time_range(
@@ -27,6 +33,11 @@ def def_orb():
     # )
 
     return iss_spacecraft.orbit
+
+def def_xleo_orb():
+
+    return Orbit.circular(Earth, 525*u.km, 55*u.deg)
+
 
 def prop_and_calc(orb, dt):
 
@@ -57,55 +68,155 @@ def lintime(start_time, end_time, interval_minutes):
     return times
 
 
-def haversine_grid(lat, lon, lats, lons):
-    # Radius of the Earth in kilometers
-    R = 6371.0
+# def haversine_grid(lat, lon, lats, lons):
+#
+#     # Difference in coordinates
+#     dlat = np.radians(lats - lat)
+#     dlon = np.radians(lons - lon)
+#
+#     # Haversine formula
+#     a = np.sin(dlat / 2) ** 2 + (np.cos(lat) * np.cos(lats) * np.sin(dlon / 2) ** 2)
+#     c = np.arcsin(np.sqrt(a))
+#
+#     # Total distance in kilometers
+#     distance = 2 * (Earth.R.value / 1000) * c
+#
+#     return distance.T
 
-    # Difference in coordinates
-    dlat = lats - lat
-    dlon = lons - lon
 
-    # Haversine formula
-    a = np.sin(dlat / 2) ** 2 + np.cos(lat) * np.cos(lats) * np.sin(dlon / 2) ** 2
-    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+def spherical_cosines(lat, lon, lats, lons):
 
-    # Total distance in kilometers
-    distance = R * c
+    lat1, lon1, lat2, lon2 = map(np.radians, [lat, lon, lats, lons])
 
-    return distance.T
+    distance = np.arccos(np.sin(lat1) * np.sin(lat2) +
+                         np.cos(lat1) * np.cos(lat2) * np.cos(lon1 - lon2)) * (Earth.R.value / 1000)  # Radius of the Earth in kilometers
 
+    return distance
+
+def _get_raw_coords(orb, t_deltas):
+    """Generates raw orbit coordinates for given epochs.
+
+    Parameters
+    ----------
+    orb : ~poliastro.twobody.Orbit
+        Orbit to be propagated
+    t_deltas : ~astropy.time.DeltaTime
+        Desired observation time
+
+    Returns
+    -------
+    raw_xyz : numpy.ndarray
+        A collection of raw cartesian position vectors
+    raw_epochs : numpy.ndarray
+        Associated epoch with previously raw coordinates
+    """
+    # Solve for raw coordinates and epochs
+    ephem = orb.to_ephem(EpochsArray(orb.epoch + t_deltas))
+    rr, vv = ephem.rv()
+    raw_xyz = CartesianRepresentation(
+        rr,
+        xyz_axis=-1,
+        differentials=CartesianDifferential(vv, xyz_axis=-1),
+    )
+    raw_epochs = ephem.epochs
+
+    return raw_xyz, raw_epochs
+
+
+def _from_raw_to_ITRS(raw_xyz, raw_obstime):
+    """Converts raw coordinates to ITRS ones.
+
+    Parameters
+    ----------
+    raw_xyz : numpy.ndarray
+        A collection of rwa position coordinates
+    raw_obstime : numpy.ndarray
+        Associated observation time
+
+    Returns
+    -------
+    itrs_xyz: ~astropy.coordinates.ITRS
+        A collection of coordinates in ITRS frame
+
+    """
+    # Build GCRS and ITRS coordinates
+    gcrs_xyz = GCRS(
+        raw_xyz,
+        obstime=raw_obstime,
+        representation_type=CartesianRepresentation,
+    )
+    itrs_xyz = gcrs_xyz.transform_to(ITRS(obstime=raw_obstime))
+
+    return itrs_xyz
+
+
+def propagate_orbit(orb, start_t, end_t):
+
+    #gt = GroundtrackPlotter()
+    t_span = time_range(
+        Time(start, format="isot", scale="utc"), periods=interval, end=Time(end, format="isot", scale="utc")
+    )
+
+    t_deltas = t_span - orb.epoch
+
+    raw_xyz, raw_obstime = _get_raw_coords(orb, t_deltas)
+    itrs_xyz = _from_raw_to_ITRS(raw_xyz, raw_obstime)
+    itrs_latlon = itrs_xyz.represent_as(SphericalRepresentation)
+
+    lat = itrs_latlon.lat.to(u.deg),
+    lon = itrs_latlon.lon.to(u.deg)
+
+    return lat, lon
+
+
+def is_sat_over_target(sat_coords, global_coords, max_off_zenith):
+
+    rad = sat_coords[2] * np.sin(np.radians(max_off_zenith))
+
+    #dist = haversine_grid(global_coords[0], global_coords[1], sat_coords[0], sat_coords[1])
+    dist = spherical_cosines(global_coords[0], global_coords[1], sat_coords[0], sat_coords[1])
+
+    return dist <= rad
+
+
+def gen_coord_grid(north_max, south_max, dlat, dlon):
+
+    # generate earth coords
+    lat_list, lon_list = [], []
+
+    lat_vec = np.arange(south_max, north_max, dlat)
+    lon_vec = np.arange(-180, 180, dlon)
+    lat_grid, lon_grid = np.meshgrid(lat_vec, lon_vec)
+
+    return lat_grid, lon_grid
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    lat_list, lon_list = [], []
 
-    lat_vec = np.radians(np.arange(-60, 60, 1))
-    lon_vec = np.radians(np.arange(-180, 180, 1))
-    lat_grid, lon_grid = np.meshgrid(lat_vec, lon_vec)
 
-    start = "2013-03-18T10:30:00"
-    end = "2024-03-18T13:30:00"
-    interval = 150
+    lat_grid, lon_grid = gen_coord_grid(60, -60, 1, 1)
+
+    # generate time
+    start = "2025-01-01T00:00:00"
+    end = "2025-01-02T00:00:00"
+    interval = 24*60
     time_list = lintime(start, end, interval)
 
-    orb = def_orb()
 
-    for t in time_list:
+    # define orbit
+    orb = def_xleo_orb()
 
-        lat, lon, _ = prop_and_calc(orb, t)
-        lat_list.append(np.degrees(lat))
-        lon_list.append(np.degrees(lon))
+    lat, lon = propagate_orbit(orb, start, end)
 
-        dist = haversine_grid(lat, lon, lat_grid, lon_grid)
+    in_range_grid = is_sat_over_target((lat[0][0].value, lon[0].value, 525), (lat_grid, lon_grid), 30)
 
-        # plt.imshow(dist, extent=(-180, 180, 60, -60))
-        # plt.scatter(np.degrees(lon), np.degrees(lat))
-        # plt.show()
+    plt.imshow(in_range_grid)
 
-
-    plt.scatter(lon_list, lat_list)
-    plt.xticks(labels=lon_vec,)
+    #plt.scatter(lon, lat)
+    #plt.xticks(labels=lon_vec,)
     plt.show()
+
+
 
 
